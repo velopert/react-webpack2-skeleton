eec5fbefd1f529e1b8164312632fbf8e43af4590
velopert <public.velopert@gmail.com>
2017-03-01 17:27:06 +0900

    remove build directory before excuting build

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -89,19 +89,7 @@ module.exports = {
         // Generates an `index.html` file with the <script> injected.
         new HtmlWebpackPlugin({
             inject: true,
-            template: paths.appHtml,
-            minify: {
-                removeComments: true,
-                collapseWhitespace: true,
-                removeRedundantAttributes: true,
-                useShortDoctype: true,
-                removeEmptyAttributes: true,
-                removeStyleLinkTypeAttributes: true,
-                keepClosingSlash: true,
-                minifyJS: true,
-                minifyCSS: true,
-                minifyURLs: true
-            }
+            template: paths.appHtml
         })
     ],
     resolve: {
