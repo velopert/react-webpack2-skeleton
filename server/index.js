208a5984ed60450c7eebfbadf79e62875517e91f
velopert <public.velopert@gmail.com>
2017-03-04 19:48:58 +0900

    server order

--- server/index.js
+++ server/index.js
@@ -3,9 +3,10 @@ const app = express();
 const path = require('path');
 const renderer = require('./renderer');
 
-app.get('/', renderer); // without this code, index page won't be server-rendered
-app.use(express.static(path.join(__dirname, '../build')));
+
 app.get('*', renderer);
+app.use(express.static(path.join(__dirname, '../build')));
+
 
 app.listen(8080, ()=>{
     console.log('Listening on port 8080');
